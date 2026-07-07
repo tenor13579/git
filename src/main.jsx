06db9f873ce70e9891e88627cdb79b522c635ea1
ideal_vista <gleenabessolo@gmail.com>
2026-07-08 03:56:13 +0500

    dix

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from './components/ui/provider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <BrowserRouter basename="/git">
+      <HashRouter>
       <Provider>
       <App />
       </Provider>
-      </BrowserRouter>
+      </HashRouter>
   </StrictMode>,
 )
